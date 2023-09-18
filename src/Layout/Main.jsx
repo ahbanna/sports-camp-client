6ecb841eca0db7099b0219d200bc7c41567d13ca
ahbanna <alhasanulbanna@gmail.com>
2023-09-18 21:08:14 +0600

    scroll to top added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import NavBar from "../pages/Shared/NavBar/NavBar";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 
 const Main = () => {
   return (
-    <div>
+    <div className="relative">
+      <ScrollToTop></ScrollToTop>
       <NavBar></NavBar>
       <Outlet></Outlet>
       <Footer></Footer>
---
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import scrollToTopIcon from "../../../src/assets/icons/football.png";
+import "./ScrollToTop.css";
+
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    const topOffset = window.pageYOffset || document.documentElement.scrollTop;
+
+    // Set visibility based on the scroll position (e.g., show the button when scrolling down 200 pixels)
+    setIsVisible(topOffset > 200);
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Add smooth scrolling animation
+    });
+  };
+
+  useEffect(() => {
+    // Add event listener for scrolling
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      // Clean up the event listener when the component unmounts
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <div className="scrollIcon">
+      {isVisible && <img src={scrollToTopIcon} alt="" onClick={scrollToTop} />}
+    </div>
+  );
+};
+
+export default ScrollToTop;
