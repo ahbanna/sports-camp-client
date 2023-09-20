e51834b4e3d08ee2b5bcce9d1076fb998e79c187
ahbanna <alhasanulbanna@gmail.com>
2023-09-20 16:20:51 +0600

    carousel btn fixed for join area

--- src/pages/Home/Join/Join.jsx
+++ src/pages/Home/Join/Join.jsx
@@ -1,28 +1,3 @@
-// import React from "react";
-// import "./Join.css";
-// import { Link } from "react-router-dom";
-
-// const Join = () => {
-//   return (
-//     <div className="join-area">
-//       <div className="join-area-bg-overlay"></div>
-//       <div className="join-area-content container">
-//         <p>Join Our Team</p>
-//         <h2>
-//           Being The Number <span>#1</span> is Our True Goals
-//         </h2>
-//         <div className="custom-btn">
-//           <button>
-//             <Link>JOIN WITH US</Link>
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Join;
-
 import React from "react";
 import "./Join.css";
 import "slick-carousel/slick/slick.css";
@@ -32,6 +7,8 @@ import img1 from "../../../assets/join/1.jpg";
 import img2 from "../../../assets/join/2.jpg";
 import img3 from "../../../assets/join/3.jpg";
 import img4 from "../../../assets/join/4.jpg";
+import img5 from "../../../assets/join/5.jpg";
+import img6 from "../../../assets/join/6.jpg";
 import { Link } from "react-router-dom";
 
 const Join = () => {
@@ -55,11 +32,11 @@ const Join = () => {
             </h2>
           </div>
           <p>
-            We are an open and friendly swimming school. Each of our little
-            floats is special and unique to us. We know each of our students not
-            only by name, but most of all we listen to them and try to get to
-            know them well. We teach swimming, but also try to pass on moral
-            principles and values.
+            Welcome to Sports camp, where passion meets purpose, and dedication
+            leads to success. If you're a sports enthusiast seeking to take your
+            skills to the next level, you're in the right place. Join our
+            community of athletes, coaches, and trainers, and embark on a
+            transformative journey towards greatness.
           </p>
           <div className="custom-btn">
             <button>
@@ -73,13 +50,19 @@ const Join = () => {
               <img src={img1} alt="" srcset="" />
             </div>
             <div>
-              <img src={img1} alt="" srcset="" />
+              <img src={img2} alt="" srcset="" />
             </div>
             <div>
-              <img src={img1} alt="" srcset="" />
+              <img src={img3} alt="" srcset="" />
             </div>
             <div>
-              <img src={img1} alt="" srcset="" />
+              <img src={img4} alt="" srcset="" />
+            </div>
+            <div>
+              <img src={img5} alt="" srcset="" />
+            </div>
+            <div>
+              <img src={img6} alt="" srcset="" />
             </div>
           </Slider>
         </div>
